5858976a0992ee2425b3e8e658b17c0ba7f2deff
Robbie Ferguson <robbie@category5.tv>
2020-07-23 09:51:06 -0400

    Reduce selectors, only update marquee if changed

--- screens/dashboard/assets/ajax/rssfeed.php
+++ screens/dashboard/assets/ajax/rssfeed.php
@@ -1,10 +1,15 @@
 <?php
 
   $feedurl = isset($_REQUEST['feed']) ? $_REQUEST['feed'] : 'https://category5.tv/feed';
-  $feed = simplexml_load_file($feedurl);
-  foreach ($feed->channel->item as $item) {
-    $title       = (string) $item->title;
-    echo $title . str_repeat('&nbsp;',10);
+  $feed = @simplexml_load_file($feedurl);
+  $count=0;
+  if (is_object($feed) && count($feed) > 0) {
+    foreach ($feed->channel->item as $item) {
+      $title       = (string) $item->title;
+      echo $title . str_repeat('&nbsp;',10);
+      if ($count++ == 20) break; // Limit the number of RSS headlines
+    }
+  } else {
+    echo 'Data feed not available.';
   }
-
 ?>
